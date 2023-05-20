3c146366c6a249440dc7d69731daa8f36511e8a9
tannukurekar <tannukurekar3@gmail.com>
2023-05-20 14:58:44 +0530

    added

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import From from './components/Form/Form';
 import Navbar from './components/Navbar/Navbar';
 
 import Home from './components/Home/Home';
-<<<<<<< HEAD
 import Grocery from './View/Grocery/Grocery';
 function App() {
   return (
    <>
    <Grocery/>
    </>
-=======
-import Jewellery from './View/Jewallary/Jewellery'
-import Electronic from './View/Electronic/Electronic-shop';
-import LoginPage from './View/Login Page/Login-page';
-function App() {
-  return (
-    <>
-      <Jewellery/>
-    </>
->>>>>>> 53d13038176e1e59ad57f536fd8b71667263f1d5
   );
 }
 
